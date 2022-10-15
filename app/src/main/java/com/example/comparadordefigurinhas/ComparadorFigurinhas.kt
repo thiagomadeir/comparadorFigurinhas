b4087eb949b00a884d9be6263064af7206ce030e
Thiago Madeira <thiagomadeir@gmail.com>
2022-10-14 22:44:49 -0300

    first commit

---
+++ app/src/main/java/com/example/comparadordefigurinhas/ComparadorFigurinhas.kt
@@ -0,0 +1,20 @@
+package com.example.comparadordefigurinhas
+
+import android.widget.EditText
+
+class ComparadorFigurinhas{
+
+    lateinit var editText: EditText
+
+    fun compararFigurinhas(repetidasUsu1: Array<String>,faltantesUsu1 : Array<String>){
+
+
+    }
+
+    fun salvarFigurinhas(){
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/comparadordefigurinhas/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.comparadordefigurinhas
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    lateinit var textView: TextView
+    lateinit var button: Button
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+
+        editText = findViewById(R.id.repetidas1)
+
+        textView = findViewById(R.id.txtViewTest)
+        button = findViewById(R.id.btnSalvar)
+
+
+
+        button.setOnClickListener {
+           var texto = editText.text.toString()
+            textView.text = texto
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
