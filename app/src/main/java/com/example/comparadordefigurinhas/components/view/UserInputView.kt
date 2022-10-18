fdf95ffcfc608f77c32bd8214037edf85a7d41d8
Thiago Madeira <thiagomadeir@gmail.com>
2022-10-17 21:14:37 -0300

    Criado classe singleton GetInputFigures

--- app/src/main/java/com/example/comparadordefigurinhas/components/view/UserInputView.kt
+++ app/src/main/java/com/example/comparadordefigurinhas/components/view/UserInputView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.comparadordefigurinhas.GetInputFigures
 import com.example.comparadordefigurinhas.R
 import com.example.comparadordefigurinhas.databinding.ViewUserInputBinding
 import com.example.comparadordefigurinhas.model.User
@@ -47,64 +48,17 @@ class UserInputView @JvmOverloads constructor(
 
     fun getMainUserValues(): User {
 
-        val missing = fillList(missingView.text.toString())
-        val repeat = fillList(repeatView.text.toString())
+        val missing = GetInputFigures.fillList(missingView.text.toString())
+        val repeat = GetInputFigures.fillList(repeatView.text.toString())
 
         return User(repeat, missing)
     }
 
     fun getGuestUserValues(): User {
-        val missing = processGuestLine(missingView.text.toString(), ", ")
-        val repeat = processGuestLine(repeatView.text.toString(), ", ")
+        val missing = GetInputFigures.processGuestLine(missingView.text.toString(), ", ")
+        val repeat = GetInputFigures.processGuestLine(repeatView.text.toString(), ", ")
 
         return User(repeat, missing)
-    }
-
-    private fun fillList(values: String): List<String> {
-
-        val lines = getLines(values)
-        Log.e("hello lines", lines.toString())
-
-        val resultList = mutableListOf<String>()
-
-        lines.forEach {
-            val line = processLine(it)
-            resultList.addAll(line)
-        }
-
-        Log.e("hello result", resultList.toString())
-
-        return resultList
-    }
-
-    private fun processLine(it: String, splitPattern: String = " "): List<String> {
-        val array = it.split(splitPattern)
-        val teamCode = array[0]
-        val teamNumbers = array.subList(1, array.size)
-        return teamNumbers.map { teamCode + it }
-    }
-
-    private fun processGuestLine(it: String, splitPattern: String = " "): List<String> {
-        Log.e("hello user guest", it)
-
-        var list = it.split(splitPattern)
-
-        val tratatedList = mutableListOf<String>()
-
-        list.forEach { m ->
-            if(m.contains("(")) {
-                tratatedList.add(m.replaceRange(m.indexOf("("), m.indexOf(")") +1, ""))
-            }
-            else {
-                tratatedList.add(m)
-            }
-        }
-        return tratatedList
-    }
-
-    private fun getLines(values: String): List<String> {
-        Log.e("hello values", values)
-        return values.split("\n")
     }
 
     fun setRepeatValue(repeat: String) {
